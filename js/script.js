4996d35ad22d9419fe096edf7ed879d6f643f45e
jooaoo26 <161589090+jooaoo26@users.noreply.github.com>
2024-06-12 07:41:27 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,17 +9,17 @@ meuFormulario.addEventListener("submit", function(event) {
 
 
 function consoante(){
-  
-  
 
-  let letra = document.getElementById("letra").value;
 
- 
+
+  let letra = document.getElementById('letra').value;
+
+
 
   if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
-    resultado_letra = A letra "${letra}" é vogal;
+    resultado_letra = `A letra '${letra}' é vogal`;
   } else {
-    resultado_letra = A letra "${letra}" é consoante;
+    resultado_letra =`A letra '${letra}' é consoante`;
   }
 
   document.getElementById("resultado_letra").value = resultado_letra;
